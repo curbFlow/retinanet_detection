0497894b3563192c0df0b08e78f3ba9c6b92495d
BitFloyd <sby.jcub@gmail.com>
2021-01-05 12:40:36 -0500

    add fsize to filename

--- eval.py
+++ eval.py
@@ -1,14 +1,12 @@
 import numpy as np
-import cv2 
+import cv2
 import pandas as pd
 import matplotlib.image as mpimg
-from slide_window.vehicle_tracking import *
 import keras
 from keras_retinanet.models.resnet import custom_objects
 from tqdm import tqdm
-from multiprocessing import Pool
-import multiprocessing
-
+import matplotlib.pyplot as plt
+import csv
 
 
 def draw_bb(img, bb_list, color=(0, 0, 255)):
@@ -71,13 +69,13 @@ class img_frame(object):
         try:
             for bb in self.bb_true:
                 cv2.rectangle(imcopy, (bb[0], bb[1]),
-                            (bb[2], bb[3]), (0, 255, 0), 10)
+                              (bb[2], bb[3]), (0, 255, 0), 10)
         except:
             pass
-        try: 
+        try:
             for bb in self.bb_pred:
                 cv2.rectangle(imcopy, (bb[0], bb[1]),
-                            (bb[2], bb[3]), (0, 0, 255), 10)
+                              (bb[2], bb[3]), (0, 0, 255), 10)
         except:
             pass
         plt.imshow(imcopy)
@@ -123,7 +121,6 @@ class img_frame(object):
             return 0, 0, len(bb_true_class)
 
 
-
 class base_evaluator(object):
     def __init__(self, csv_path):
         """
@@ -131,9 +128,8 @@ class base_evaluator(object):
         img_path, x1, y1, x2, y2, class
         """
         self.num_classes = 1
-        self.class_map = {'car': 0, 'truck': 0, 'pedestrian': -1}
-        self.db = pd.read_csv(
-            csv_path, names=['Frame', 'x1', 'y1', 'x2', 'y2', 'Label'])
+        self.class_map = {'person': 0, 'vehicle': 1}
+        self.db = pd.read_csv(csv_path, names=['Frame', 'x1', 'y1', 'x2', 'y2', 'Label'])
         self.db['Label'] = self.db['Label'].apply(lambda x: self.class_map[x])
         self.db = self.db[self.db['Label'] >= 0]
         self.detector = None
@@ -164,73 +160,23 @@ class base_evaluator(object):
         print('recall:{}'.format(res_sum[0] / (res_sum[0] + res_sum[2])))
 
 
-def apply_detection(frame):
-    img = mpimg.imread(frame.frame_name)
-    frame.bb_pred = vehicle_detector(resize_scale=0.5).detect(img)
-    return 
-
-class slide_window_eval(base_evaluator):
-    def __init__(self, csv_path):
-        super(slide_window_eval, self).__init__(csv_path)
-        self.detector = vehicle_detector(resize_scale=0.5).detect
-    
-
-
-class rtna_coco_eval(base_evaluator):
-    def __init__(self, csv_path):
-        self.num_classes = 3
-        self.class_map = {'car': 2, 'truck': 7, 'pedestrian': 0}
-        self.db = pd.read_csv(
-            csv_path, names=['Frame', 'x1', 'y1', 'x2', 'y2', 'Label'])
-        self.db['Label'] = self.db['Label'].apply(lambda x: self.class_map[x])
-        self.db = self.db[self.db['Label'] >= 0]
-        self.detector = self.detect
-        self.frames = []
-        self.coco_model = None
-        self.scale=0.5
-
-    def load_model(self):
-        self.coco_model = keras.models.load_model(
-            'snapshots/resnet50_coco_best.h5', custom_objects=custom_objects)
-
-    def detect(self, img):
-        if not self.coco_model:
-            self.load_model()
-        scale = self.scale
-        bb_list = []
-        img_scaled = cv2.resize(img, None, fx=scale, fy=scale)
-        _, _, detections = self.coco_model.predict_on_batch(
-            np.expand_dims(img_scaled, axis=0))
-        predicted_labels = np.argmax(detections[0, :, 4:], axis=1)
-        scores = detections[0, np.arange(
-            detections.shape[1]), 4 + predicted_labels]
-        detections[:, :4] /= scale
-        for idx, (label, score) in enumerate(zip(predicted_labels, scores)):
-            if score < 0.5:
-                continue
-            b = detections[0, idx, :4].astype(int)
-            bb_list.append([b[0], b[1], b[2], b[3], label])
-        return np.array(bb_list)
-
-
 class retinanet_eval(base_evaluator):
-    def __init__(self, csv_path, model_path):
+    def __init__(self, csv_path, model_path, classes_csv):
         self.num_classes = 3
-        self.class_map = {'car': 0, 'truck': 1, 'pedestrian': 2}
-        self.db = pd.read_csv(
-            csv_path, names=['Frame', 'x1', 'y1', 'x2', 'y2', 'Label'])
+        with open(classes_csv, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            self.class_map = {labelname: int(idx) for labelname, idx in reader}
+        self.db = pd.read_csv(csv_path, names=['Frame', 'x1', 'y1', 'x2', 'y2', 'Label'])
         self.db['Label'] = self.db['Label'].apply(lambda x: self.class_map[x])
         self.db = self.db[self.db['Label'] >= 0]
         self.detector = self.detect
         self.frames = []
-        self.model_path=model_path
+        self.model_path = model_path
         self.model = None
-        self.scale = 0.5
-
+        self.scale = 1.0
 
     def load_model(self):
-        self.model = keras.models.load_model(
-            self.model_path, custom_objects=custom_objects)
+        self.model = keras.models.load_model(self.model_path,custom_objects=custom_objects)
 
     def detect(self, img):
         if not self.model:
@@ -238,8 +184,7 @@ class retinanet_eval(base_evaluator):
         scale = self.scale
         bb_list = []
         img_scaled = cv2.resize(img, None, fx=scale, fy=scale)
-        _, _, detections = self.model.predict_on_batch(
-            np.expand_dims(img_scaled, axis=0))
+        _, _, detections = self.model.predict_on_batch(np.expand_dims(img_scaled, axis=0))
         predicted_labels = np.argmax(detections[0, :, 4:], axis=1)
         scores = detections[0, np.arange(
             detections.shape[1]), 4 + predicted_labels]
@@ -250,5 +195,5 @@ class retinanet_eval(base_evaluator):
             b = detections[0, idx, :4].astype(int)
             bb_list.append([b[0], b[1], b[2], b[3], label])
         return np.array(bb_list)
-        
+
 
--- train_mobilnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python train_mobilenet.py --alpha 1.0 --batch-size 8
-sleep 5s
-python train_mobilenet.py --alpha 0.75 --batch-size 8
-sleep 5s
-python train_mobilenet.py --alpha 0.5 --batch-size 8
-sleep 5s
-python train_mobilenet.py --alpha 0.25 --batch-size 8
